da3c7553d4886122bd45a81a9730b01e551137ae
Nick Brown <nick@nickbrown.me>
2014-03-09 04:46:34 -0700

    added restful route; generated controller; wrote store method

---
+++ app/controllers/ProductsController.php
@@ -0,0 +1,87 @@
+<?php
+
+class ProductsController extends \BaseController {
+
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @return Response
+	 */
+	public function index()
+	{
+		//
+	}
+
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return Response
+	 */
+	public function create()
+	{
+		//
+	}
+
+	/**
+	 * Store a newly created resource in storage.
+	 *
+     * @param $id
+     *
+	 * @return Response
+	 */
+	public function store($id)
+	{
+        $product = Product::findOrFail($id);
+
+        return Response::json(array(
+            'error' => false,
+            'products' => $product->toArray(),
+            200
+        ));
+	}
+
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function show($id)
+	{
+		//
+	}
+
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function edit($id)
+	{
+		//
+	}
+
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function update($id)
+	{
+		//
+	}
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function destroy($id)
+	{
+		//
+	}
+
+}
