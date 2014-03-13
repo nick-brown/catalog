cb108a63fd4f432967c43b746e18fb1bfa2813fc
Nick Brown <nick@nickbrown.me>
2014-03-12 22:11:23 -0700

    added all product fields to view

--- app/views/home/index.blade.php
+++ app/views/home/index.blade.php
@@ -11,7 +11,21 @@
 
             <div style="float:left;">
                 <form>
-                    <input type="text" ng-model="editing.name" >
+                    <input type="text" ng-model="editing.name">
+                    <br>
+                    <input type="text" ng-model="editing.width">
+                    <br>
+                    <input type="text" ng-model="editing.length">
+                    <br>
+                    <input type="text" ng-model="editing.height">
+                    <br>
+                    <input type="text" ng-model="editing.weight">
+                    <br>
+                    <input type="text" ng-model="editing.weight">
+                    <br>
+                    <input type="text" ng-model="editing.usd_value">
+                    <br>
+                    <textarea ng-model="editing.description"></textarea>
                     <br>
                     <input type="submit" value="{{ editing.name ? 'Update' : 'Add' }}" ng-click="addUpdateProduct()">
                 </form>
