983e7347874b12594dd670c7d787027782d909f1
Nick Brown <nick@nickbrown.me>
2014-03-13 00:57:00 -0700

    altered product factory; shortened assignments

--- public/js/app.js
+++ public/js/app.js
@@ -1,49 +1,38 @@
-'use strict';
 
-var productEditor = angular.module('productEditor', ['ngResource'])
-    .controller('HomeCtrl', ['$scope', 'productFactory', function($scope, productFactory) {
-        $scope.products = productFactory.query();
+angular.module('productEditor', ['ngResource'])
+    .controller('HomeCtrl', ['$scope', 'Product', function($scope, Product) {
+        'use strict';
+
+        $scope.products = Product.query();
         $scope.editing = {};
 
         $scope.addUpdateProduct = function() {
-            var product = $scope.currentProduct;
 
-            if(product) {
-                product.name = $scope.editing.name;
-                product.width = $scope.editing.width;
-                product.length = $scope.editing.length;
-                product.height = $scope.editing.height;
-                product.weight = $scope.editing.weight;
-                product.usd_value = $scope.editing.usd_value;
-                product.description = $scope.editing.description;
+            if($scope.currentProduct) {
+                $scope.currentProduct = $scope.editing;
             } else {
-                $scope.products.push({
-                    name: $scope.editing.name,
-                    width: $scope.editing.width,
-                    length: $scope.editing.length,
-                    height: $scope.editing.height,
-                    weight: $scope.editing.weight,
-                    usd_value: $scope.editing.usd_value,
-                    description: $scope.editing.description
+                var newProduct = new Product($scope.editing);
+
+                newProduct.$save().then(function() {
+                    $scope.products.push(newProduct);
+
+                    $scope.editing = {};
+                },
+                function(response) {
+                    console.log(response.error.type + ': ' + response.error.message);
                 });
             }
         };
 
         $scope.listChange = function() {
             if($scope.currentProduct) {
-                $scope.editing.name = $scope.currentProduct.name;
-                $scope.editing.width = $scope.currentProduct.width;
-                $scope.editing.length = $scope.currentProduct.length;
-                $scope.editing.height = $scope.currentProduct.height;
-                $scope.editing.weight = $scope.currentProduct.weight;
-                $scope.editing.usd_value = $scope.currentProduct.usd_value;
-                $scope.editing.description = $scope.currentProduct.description;
+                $scope.editing = $scope.currentProduct;
             } else {
                 $scope.editing = {};
             }
         }
     }])
-    .factory('productFactory', function($resource) {
-            return $resource('/catalog/public/products/:productId', { productId: '@id' }
+    .factory('Product', function($resource) {
+            return $resource('/catalog/public/products/:productId', { productId: '@id'}
         );
     });
